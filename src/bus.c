08bdc75e5553d6371816b184d71bb38bf29db0d3
xlanor <contact@jingk.ai>
2026-04-01 20:37:51 +0100

    preparing to refactor everything now that the first bit of nestest passes
    Signed-off-by: xlanor <contact@jingk.ai>

--- src/bus.c
+++ src/bus.c
@@ -1,16 +1,19 @@
 #include "bus.h"
 #include <string.h>
 
-static uint8_t ram[0x10000]; // Flat 64KB for now
 
-void bus_init(void) {
-    memset(ram, 0, sizeof(ram));
+void bus_init(Bus * bus) {
+    memset(bus->ram, 0, sizeof(bus->ram));
+    bus ->tick = NULL;
+    bus->tick_ctx = NULL;
 }
 
-uint8_t bus_read(uint16_t addr) {
-    return ram[addr];
+uint8_t bus_read(Bus *bus, uint16_t addr) {
+    if (bus->tick) bus->tick(bus->tick_ctx);
+    return bus->ram[addr];
 }
 
-void bus_write(uint16_t addr, uint8_t val) {
-    ram[addr] = val;
+void bus_write(Bus *bus, uint16_t addr, uint8_t val) {
+    if (bus->tick) bus->tick(bus->tick_ctx);
+    bus->ram[addr] = val;
 }
--- src/bus.h
+++ src/bus.h
@@ -3,8 +3,16 @@
 
 #include <stdint.h>
 
-void    bus_init(void);
-uint8_t bus_read(uint16_t addr);
-void    bus_write(uint16_t addr, uint8_t val);
+typedef void (*bus_tick_fn)(void *ctx);
+typedef struct {
+    // probably need to change this to match actual NES ram later.
+    uint8_t ram[0x10000];
+    bus_tick_fn tick;
+    void *tick_ctx;
+} Bus;
+
+void bus_init(Bus* bus);
+uint8_t bus_read(Bus *bus, uint16_t addr);
+void bus_write(Bus *bus, uint16_t addr, uint8_t val);
 
 #endif
